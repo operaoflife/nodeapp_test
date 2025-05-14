6549871489838b8f6715bfa6e63a6937018a107a
operaoflife <108854859+operaoflife@users.noreply.github.com>
2025-05-14 16:48:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Reema" }');
+    res.send('{ "response": "welcome to kloudmate" }');
 });
 
 app.get('/will', function (req, res) {
